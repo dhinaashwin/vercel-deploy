88a6cece0ade3bc683a6c3b6099a728d5a385329
dhinaashwin <120519242+dhinaashwin@users.noreply.github.com>
2024-06-29 02:20:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,22 +10,6 @@ app.use(cors({
   methods:['POST','GET'],
   credentials:true }));
 mongoose.connect('mongodb+srv://dhinaashwin11:Mongopassword@cluster0.iyp2kv4.mongodb.net/new-base?retryWrites=true&w=majority&appName=Cluster0')
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
 const handler = (req, res) => {
   const d = new Date()
